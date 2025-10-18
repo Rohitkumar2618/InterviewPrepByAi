e3fc720a5f8aba00e87a02fcc47fdb9ab1042ba0
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2025-10-19 00:27:32 +0530

    project initialised

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const Authayout = ( { children }:{ children: ReactNode } ) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Authayout
